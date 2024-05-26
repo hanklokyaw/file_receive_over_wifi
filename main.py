4c724de02d73e42a6acef71fd803ce5fcaa25a42
hboyz <hank.lo.kyaw@gmail.com>
2024-05-26 09:27:34 -0700

    init

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# from flask import Flask, request, redirect, url_for, flash
+# from flask import Flask, request, redirect, url_for, flash, jsonify, render_template, send_from_directory
 # from werkzeug.utils import secure_filename
 # import os
 #
@@ -18,31 +18,38 @@
 #     return '.' in filename and \
 #            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 #
-# @app.route('/', methods=['GET', 'POST'])
-# def upload_file():
-#     if request.method == 'POST':
-#         # Check if the post request has the file part
-#         if 'file' not in request.files:
-#             flash('No file part')
-#             return redirect(request.url)
-#         file = request.files['file']
-#         # If user does not select file, browser also submits an empty part without filename
-#         if file.filename == '':
-#             flash('No selected file')
-#             return redirect(request.url)
+# @app.route('/')
+# def upload_form():
+#     return render_template('index.html')
+#
+# @app.route('/upload', methods=['POST'])
+# def upload_files():
+#     if 'files' not in request.files:
+#         flash('No file part')
+#         return redirect(url_for('upload_form'))
+#
+#     files = request.files.getlist('files')
+#     if not files or all(file.filename == '' for file in files):
+#         flash('No selected file')
+#         return redirect(url_for('upload_form'))
+#
+#     for file in files:
 #         if file and allowed_file(file.filename):
 #             filename = secure_filename(file.filename)
 #             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-#             return 'File successfully uploaded'
-#     return '''
-#     <!doctype html>
-#     <title>Upload new File</title>
-#     <h1>Upload new File</h1>
-#     <form method=post enctype=multipart/form-data>
-#       <input type=file name=file>
-#       <input type=submit value=Upload>
-#     </form>
-#     '''
+#
+#     return jsonify({'message': 'Files successfully uploaded'})
+#
+# @app.route('/download')
+# def index():
+#     # Get the list of files in the 'uploads' directory
+#     files = os.listdir('uploads')
+#     return render_template('download.html', files=files)
+#
+# @app.route('/download/<filename>')
+# def download_file(filename):
+#     # Serve the selected file for download
+#     return send_from_directory('uploads', filename)
 #
 # if __name__ == '__main__':
 #     app.run(debug=True, host='0.0.0.0', port=5000)
@@ -50,12 +57,10 @@
 
 
 
-from flask import Flask, request, redirect, url_for, flash, jsonify, render_template
-from werkzeug.utils import secure_filename
+from flask import Flask, redirect, url_for, render_template, send_from_directory
 import os
 
 app = Flask(__name__)
-app.secret_key = 'supersecretkey'
 
 # Configure upload folder and allowed extensions
 UPLOAD_FOLDER = 'uploads'
@@ -63,34 +68,33 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'mp4', 'mov'}
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
-
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
-def upload_form():
+def index():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
-def upload_files():
-    if 'files' not in request.files:
-        flash('No file part')
-        return redirect(url_for('upload_form'))
+@app.route('/upload')
+def upload_form():
+    return render_template('upload.html')
 
-    files = request.files.getlist('files')
-    if not files or all(file.filename == '' for file in files):
-        flash('No selected file')
-        return redirect(url_for('upload_form'))
+@app.route('/download')
+def download_form():
+    # Get the list of files in the 'uploads' directory
+    files = os.listdir('uploads')
+    return render_template('download.html', files=files)
 
-    for file in files:
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+@app.route('/upload_file', methods=['POST'])
+def upload_file():
+    # Handle file upload logic here
+    return redirect(url_for('upload_form'))
 
-    return jsonify({'message': 'Files successfully uploaded'})
+@app.route('/download/<filename>')
+def download_file(filename):
+    # Serve the selected file for download
+    return send_from_directory('uploads', filename)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
